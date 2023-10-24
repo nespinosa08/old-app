b019734202bc01f4954a4d5ed3e8e2cc65c6af0a
nespinosa08 <nespinosa08@gmail.com>
2023-10-24 18:16:12 -0500

    app basico

---
+++ src/assets/component/Square.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+export const Square = ({value, onSquareClick}) => {
+
+  return (
+    <button className='square' onClick={onSquareClick}>{value}</button>
+  )
+}
