853ff54a9138c8268aaaa8ee9d019ea4aa419b7a
nespinosa08 <nespinosa08@gmail.com>
2023-10-24 19:07:57 -0500

    father component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-import { Square } from './assets/component/Square'
-import { calculateWinner } from './assets/helpers/calculateWinner';
+import { Board } from './assets/component/Board';
 
 
 export const App = () => {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-    
-  const handleClick = (i)=>{
-    if (squares[i] || winner) {
-      return
-    } 
-    const nextSquares = squares.slice();
-    if  (xIsNext){
-      nextSquares[i]='X'
-    } else{
-      nextSquares[i]='O'
-    }
-      setSquares(nextSquares);
-      setXIsNext(!xIsNext);
-  };
-  
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner){
-    status = `GANADOR: ${winner}`
-  } else{
-    status = `Juega ${xIsNext? 'X': 'O'}`
-  }
-  
   
   return (
     <>
-      <h1>ViejaAPP</h1>
-  
-      <h2 className='status'>{status}</h2>
-  
-      <div className='board-row'>
-      <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-      <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
-      </div>
-  
-      <div className='board-row'>
-      <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
-      </div>
-      
-      <div className='board-row'>
-      <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
-      </div>
+      <Board />
     </>
   )
 }
